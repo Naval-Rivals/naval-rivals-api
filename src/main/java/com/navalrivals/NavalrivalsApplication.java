2f1bddf368067eabf77cd5a81616f65dd81d3a19
Caio de Souza <caiodesouza.cds@gmail.com>
2026-06-25 10:20:25 -0300

    start: commit inicial

---
+++ src/main/java/com/navalrivals/NavalrivalsApplication.java
@@ -0,0 +1,13 @@
+package com.navalrivals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavalrivalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavalrivalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navalrivals/NavalrivalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navalrivals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavalrivalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
