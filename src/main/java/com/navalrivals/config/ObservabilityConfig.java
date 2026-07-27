e5054f9af0816a1985f28dd408b2a275e15f1957
Caio de Souza <caiodesouza.cds@gmail.com>
2026-07-27 11:26:07 -0300

    fix: ajusta registro de traces para excluir as req do actuator

---
+++ src/main/java/com/navalrivals/config/ObservabilityConfig.java
@@ -0,0 +1,25 @@
+package com.navalrivals.config;
+
+import io.micrometer.observation.ObservationPredicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
+
+@Configuration
+public class ObservabilityConfig {
+
+    @Bean
+    public ObservationPredicate noActuatorObservations(){
+        PathMatcher pathMatcher = new AntPathMatcher();
+        return (name, context) -> {
+            if (context instanceof ServerRequestObservationContext serverContext){
+                String path = serverContext.getCarrier().getRequestURI();
+                return !pathMatcher.match("/actuator/**", path);
+
+            }
+            return true;
+        };
+    }
+}
