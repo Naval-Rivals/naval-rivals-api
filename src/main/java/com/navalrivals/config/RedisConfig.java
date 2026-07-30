bff15d4683bb9e2aa3cc9e062b60bb16fc073e9f
Caio de Souza <caiodesouza.cds@gmail.com>
2026-07-30 15:11:18 -0300

    feat: add config para o game storage

--- src/main/java/com/navalrivals/config/RedisConfig.java
+++ src/main/java/com/navalrivals/config/RedisConfig.java
@@ -1,10 +1,12 @@
 package com.navalrivals.config;
 
+import com.navalrivals.domain.game.entity.Game;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -24,6 +26,18 @@ public class RedisConfig {
                 .serializeValuesWith(RedisSerializationContext.SerializationPair
                         .fromSerializer(GenericJacksonJsonRedisSerializer.builder().build()));
 
-        return RedisCacheManager.builder(connectionFactory).cacheDefaults(config).build();
+        return RedisCacheManager
+                .builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
+    }
+
+    @Bean
+    public RedisTemplate<String, Game> ganeRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Game> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(GenericJacksonJsonRedisSerializer.builder().build());
+        return template;
     }
 }
